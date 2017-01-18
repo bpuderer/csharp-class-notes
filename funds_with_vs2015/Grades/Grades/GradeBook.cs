f3b70ae9f933b15a69da5f40db4c222c19184885
Ben Puderer <bpuderer@yahoo.com>
2017-01-17 21:44:35 -0500

    initial commit

---
+++ funds_with_vs2015/Grades/Grades/GradeBook.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    public class GradeBook : GradeTracker
+    {
+        public GradeBook()
+        {
+            _name = "Empty";
+            grades = new List<float>();
+        }
+
+        public override GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+            float sum = 0;
+            foreach (float grade in grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+            stats.AverageGrade = sum / grades.Count;
+            return stats;
+        }
+        
+        public override IEnumerator GetEnumerator()
+        {
+            return grades.GetEnumerator();
+        }
+
+        //List<float> grades = new List<float>(); //field initializer syntax. create field and initialize
+        //protected - accessible in this class or derived classes
+        protected List<float> grades;
+
+        public override void WriteGrades(TextWriter destination)
+        {
+            foreach (float grade in grades)
+            {
+                destination.WriteLine(grade);
+            }
+        }
+
+        public override void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+    }
+}
---
+++ funds_with_vs2015/Grades/Grades/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SpeechSynthesizer synth = new SpeechSynthesizer();
+            //synth.Speak("Hello, this is the grade book program");
+
+            // delegates can reference multiple methods.  "multi-cast delegates"
+            //book.NameChanged += new NameChangedDelegate(OnNamedChanged);
+            //book.NameChanged += new NameChangedDelegate(OnNamedChanged2);
+            // equivalent.  easier on eyes
+            // book.NameChanged += OnNamedChanged;
+
+            //book.Name = "Ben's grade book";
+            //book.Name = null;  will cause ArgumentException to be thrown
+            //book.Name = "Ben's grade book";
+
+            IGradeTracker book = CreateGradeBook();
+
+            GetBookName(book);
+            AddGrades(book);
+            SaveGrades(book);
+            WriteResults(book);
+        }
+
+        private static GradeBook CreateGradeBook()
+        {
+            //return new GradeBook();
+            return new ThrowAwayGradeBook();
+        }
+
+        private static void WriteResults(IGradeTracker book)
+        {
+            GradeStatistics stats = book.ComputeStatistics();
+
+            foreach (float grade in book)
+            {
+                Console.WriteLine(grade);
+            }
+
+            WriteResult("Average", stats.AverageGrade);
+            WriteResult("Highest", (int)stats.HighestGrade);
+            WriteResult("Lowest", stats.LowestGrade);
+            WriteResult(stats.Description, stats.LetterGrade);
+        }
+
+        private static void SaveGrades(IGradeTracker book)
+        {
+            // book.WriteGrades(Console.Out);
+            using (StreamWriter outputFile = File.CreateText("grades.txt"))
+            {
+                book.WriteGrades(outputFile);
+            }
+        }
+
+        private static void AddGrades(IGradeTracker book)
+        {
+            book.AddGrade(91);
+            book.AddGrade(89.5f);      // f = float
+            book.AddGrade(75);
+        }
+
+        private static void GetBookName(IGradeTracker book)
+        {
+            Console.WriteLine("Enter a name:");
+            try
+            {
+                book.Name = Console.ReadLine();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong!");
+            }
+        }
+
+        static void WriteResult(string description, string result)
+        {
+            Console.WriteLine($"{description}: {result}");
+        }
+
+        static void WriteResult(string description, int result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+
+        static void WriteResult(string description, float result)
+        {
+            //Console.WriteLine("{0}: {1:F2}", description, result);
+            Console.WriteLine($"{description}: {result:F2}");
+        }
+
+        static void OnNamedChanged(object sender, NameChangedEventArgs args)
+        {
+            Console.WriteLine($"Grade book changing name from {args.ExistingName} to {args.NewName}");
+        }
+    }
+}
